09c375f417a97fed1e930eef6cc16db4f416a31b
Aleksei Magusev <lexmag@me.com>
2017-02-05 22:52:52 +0100

    WIP

--- lib/xandra/cluster/control_connection.ex
+++ lib/xandra/cluster/control_connection.ex
@@ -8,7 +8,7 @@ defmodule Xandra.Cluster.ControlConnection do
   @default_timeout 5_000
   @socket_options [packet: :raw, mode: :binary, active: false]
 
-  defstruct [:socket, :address, :port, :cluster, buffer: <<>>]
+  defstruct [:address, :port, :cluster, :socket, new: true, buffer: <<>>]
 
   def start_link(cluster, address, port) do
     state = %__MODULE__{cluster: cluster, address: address, port: port}
@@ -19,22 +19,22 @@ defmodule Xandra.Cluster.ControlConnection do
     {:connect, :init, state}
   end
 
-  def connect(action, %__MODULE__{address: address, port: port} = state) do
+  def connect(_action, %__MODULE__{address: address, port: port} = state) do
     case :gen_tcp.connect(address, port, @socket_options, @default_timeout) do
       {:ok, socket} ->
-        send(self(), {:activate, action})
+        send(self(), :activate)
         {:ok, %{state | socket: socket}}
       {:error, _reason} ->
         {:backoff, 5_000, state}
     end
   end
 
-  def handle_info({:activate, action}, %__MODULE__{socket: socket} = state) do
+  def handle_info(:activate, %__MODULE__{socket: socket} = state) do
     with {:ok, supported_options} <- Utils.request_options(socket),
          :ok <- startup_connection(socket, supported_options),
          :ok <- register_to_events(socket),
          :ok <- :inet.setopts(socket, active: :true),
-         {:ok, state} <- report_active(action, state) do
+         {:ok, state} <- report_active(state) do
       {:noreply, state}
     else
       {:error, reason} -> {:disconnect, {:error, reason}, state}
@@ -59,15 +59,15 @@ defmodule Xandra.Cluster.ControlConnection do
     {:connect, :reconnect, %{state | socket: nil, buffer: <<>>}}
   end
 
-  defp report_active(:init, %{cluster: cluster, socket: socket} = state) do
-    with {:ok, {address, port}} <- :inet.peername(socket) do
-      Xandra.Cluster.start_pool(cluster, address, port)
-      %{state | address: address}
-    end
+  defp report_active(%{new: false} = state) do
+    {:ok, state}
   end
 
-  defp report_active(_action, state) do
-    {:ok, state}
+  defp report_active(%{new: true, cluster: cluster, socket: socket} = state) do
+    with {:ok, {address, port}} <- :inet.peername(socket) do
+      Xandra.Cluster.start_pool(cluster, address, port)
+      {:ok, %{state | new: false, address: address}}
+    end
   end
 
   defp startup_connection(socket, supported_options) do
